ed77ec5634e0201d3a2b1ed96b07fd90143af828
Omr <64219662+akyuzf@users.noreply.github.com>
2021-01-17 23:18:53 -0500

    Update A02_HelloWorld2.java

--- src/main/java/Part1/A02_HelloWorld2.java
+++ src/main/java/Part1/A02_HelloWorld2.java
@@ -7,7 +7,7 @@ Write a statement that prints "Hello world" to the screen. Output must exactly m
 public class A02_HelloWorld2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world2");
+        System.out.println("Hello, world2");
 
     }
 
