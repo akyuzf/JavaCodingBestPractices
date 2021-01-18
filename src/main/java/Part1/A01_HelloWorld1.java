ed381eda6f6ac08111b24ed7eff71b22bd5bd9cf
akyuzf <akyomr.us@gmail.com>
2021-01-17 23:21:22 -0500

    commit

--- src/main/java/Part1/A01_HelloWorld1.java
+++ src/main/java/Part1/A01_HelloWorld1.java
@@ -7,7 +7,7 @@ Write a statement that prints "Hello, world" to the screen. Output must exactly 
 public class A01_HelloWorld1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, world1");
 
     }
 }
