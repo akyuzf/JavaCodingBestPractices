480518f879eda2850047d93a1e3fc14f96496e74
akyuzf <akyomr.us@gmail.com>
2021-01-17 23:12:03 -0500

    commit

--- src/main/java/Part1/A01_HelloWorld1.java
+++ src/main/java/Part1/A01_HelloWorld1.java
@@ -1,13 +1,13 @@
 package Part1;
 
 /*
-Write a statement that prints "Hello world" to the screen. Output must exactly match the requirement
+Write a statement that prints "Hello, world" to the screen. Output must exactly match the requirement
  */
 
 public class A01_HelloWorld1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world1");
+        System.out.println("Hello, world");
 
     }
 }
